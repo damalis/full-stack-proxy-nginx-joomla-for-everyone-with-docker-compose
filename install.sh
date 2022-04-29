23419c43fb6aa56bb2cad03deffb9115aec84321
Erdal ALTIN <erdalaltin@gmail.com>
2022-04-29 12:04:06 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -240,7 +240,17 @@ if [ -x "$(command -v docker)" ] && [ -x "$(command -v docker-compose)" ]; then
 				echo "waiting for Let's Encrypt certificates for $domain_name"
 				sleep 5s & wait ${!}
 				if sudo [ -d "./certbot/live/$domain_name" ]; then break; fi
+			done
+			echo "Ok."
+			until [ ! -z `docker ps -q -f "status=running" --no-trunc | grep $(docker-compose ps -q proxy)` ]; do
+				echo "waiting starting proxy container"
+				sleep 2s & wait ${!}
+				if [ ! -z `docker ps -q -f "status=running" --no-trunc | grep $(docker-compose ps -q proxy)` ]; then break; fi
 			done			
+			echo ""
+			echo "Reloading Proxy Nginx ssl configuration"
+			docker exec proxy nginx -s reload > /dev/null 2>&1            			 
+			echo "Ok."
 			echo ""
 			echo "completed setup"
 			echo ""
